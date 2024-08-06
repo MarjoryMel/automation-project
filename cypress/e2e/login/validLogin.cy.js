756b61e2571ff5813ebf9623479c1bde74c5dd06
Marjory Mel <marjorymel48@gmail.com>
2024-08-06 15:46:06 -0300

    created test case two: login user with correct email and password

---
+++ cypress/e2e/login/validLogin.cy.js
@@ -0,0 +1,40 @@
+describe('Valid Login', () => {
+
+    it('checks if the home page is visible successfully', () => {
+        // Navigate to the URL
+        cy.visit('https://automationexercise.com/')
+
+        // Verify that the home page is visible
+        cy.get('a > img').should('be.visible')
+    })
+
+    it('click on the Sign Up/Log In button and check if Login to your account is visible', () => {
+        cy.visit('https://automationexercise.com/')
+
+        // Click on 'Signup / Login' button
+        cy.get('.shop-menu > .nav > :nth-child(4) > a')
+          .invoke('removeAttr', 'target')
+          .click()
+
+        // Verify 'Login to your account' is visible
+        cy.contains('Login to your account')
+          .should('be.visible')
+    })
+
+    it('the user enters their name and email', () => {
+        cy.visit('https://automationexercise.com/login')
+
+        // Use data from the fixture for user information
+        cy.fixture('userValid').then((user) => {
+            cy.get('[data-qa="login-email"]').type(user.email)
+            cy.get('[data-qa="login-password"]').type(user.password)
+            cy.get('[data-qa="login-button"]').click()
+
+            // Verify that 'ENTER ACCOUNT INFORMATION' is visible
+            cy.get(':nth-child(10) > a').should('be.visible')
+        })
+
+        //9. Click 'Delete Account' button
+        //10. Verify that 'ACCOUNT DELETED!' is visible
+    })
+})
